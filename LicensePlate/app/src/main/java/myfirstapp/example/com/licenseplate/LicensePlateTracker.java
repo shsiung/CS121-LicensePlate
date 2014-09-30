ed2bd0c02528056aa6dd1e1219db91538a515b91
JerryH <jerry8050@wl-210-144.wpa.claremont.edu>
2014-09-29 21:41:31 -0700

    add camera feature (can open native camera app but crash after)

--- LicensePlate/app/src/main/java/myfirstapp/example/com/licenseplate/LicensePlateTracker.java
+++ LicensePlate/app/src/main/java/myfirstapp/example/com/licenseplate/LicensePlateTracker.java
@@ -1,20 +1,41 @@
 package myfirstapp.example.com.licenseplate;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LicensePlateTracker extends Activity {
 
+    Button getPicButton;
+    ImageView imgFavorite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license_plate_tracker);
+
+        getPicButton = (Button)findViewById(R.id.getPicture);
     }
 
+    public void open(){
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        imgFavorite.setImageBitmap(bp);
+    }
+
+    // Default functions
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -22,6 +43,7 @@ public class LicensePlateTracker extends Activity {
         return true;
     }
 
+    // Default functions
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -33,4 +55,11 @@ public class LicensePlateTracker extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    /** Called when the user clicks the Send button */
+    public void getPicture(View view) {
+        open();
+    }
+
+
 }
