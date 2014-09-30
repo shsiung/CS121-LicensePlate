0c99e2e0065070c1ef356dbbf511c5c70e8f1494
JerryH <jerry8050@wl-210-144.wpa.claremont.edu>
2014-09-29 22:46:46 -0700

    new activity for in-app camera

--- LicensePlate/app/src/main/java/myfirstapp/example/com/licenseplate/LicensePlateTracker.java
+++ LicensePlate/app/src/main/java/myfirstapp/example/com/licenseplate/LicensePlateTracker.java
@@ -2,83 +2,19 @@ package myfirstapp.example.com.licenseplate;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
+
 
 public class LicensePlateTracker extends Activity {
-
-    Button getPicButton;
-    ImageView imgFavorite;
-
-    private Camera cameraObject;
-    private ShowCamera showCamera;
-    private ImageView pic;
-
-    public static Camera isCameraAvailiable(){
-        Camera object = null;
-        try {
-            object = Camera.open(0);
-            Log.d("ADebugTag","Camera open!");
-        }
-        catch (Exception e){
-            Log.d("ADebugTag","Camera not available!");
-        }
-        return object;
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license_plate_tracker);
-
-        pic = (ImageView)findViewById(R.id.imageView1);
-        cameraObject = isCameraAvailiable();
-        //cameraObject = Camera.open();
-        showCamera = new ShowCamera(this, cameraObject);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(showCamera);
     }
-
-    public void snapIt(View view){
-        cameraObject.takePicture(null, null, capturedIt);
-    }
-
-    private Camera.PictureCallback capturedIt = new Camera.PictureCallback() {
-
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-
-            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-            if(bitmap==null){
-                Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
-                Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();
-            }
-            cameraObject.release();
-        }
-    };
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bp = (Bitmap) data.getExtras().get("data");
-        imgFavorite.setImageBitmap(bp);
-    }
-
     // Default functions
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,6 +34,13 @@ public class LicensePlateTracker extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void getPicture(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, getPicActivity.class);
+        startActivity(intent);
     }
 
 }
