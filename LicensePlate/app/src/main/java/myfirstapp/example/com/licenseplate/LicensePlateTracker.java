b840d1a8bb7bb6a11107709f060b39c19ef24b85
JerryH <jerry8050@wl-210-144.wpa.claremont.edu>
2014-09-29 22:27:51 -0700

    camera preview in view

--- LicensePlate/app/src/main/java/myfirstapp/example/com/licenseplate/LicensePlateTracker.java
+++ LicensePlate/app/src/main/java/myfirstapp/example/com/licenseplate/LicensePlateTracker.java
@@ -3,29 +3,73 @@ package myfirstapp.example.com.licenseplate;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LicensePlateTracker extends Activity {
 
     Button getPicButton;
     ImageView imgFavorite;
+
+    private Camera cameraObject;
+    private ShowCamera showCamera;
+    private ImageView pic;
+
+    public static Camera isCameraAvailiable(){
+        Camera object = null;
+        try {
+            object = Camera.open(0);
+            Log.d("ADebugTag","Camera open!");
+        }
+        catch (Exception e){
+            Log.d("ADebugTag","Camera not available!");
+        }
+        return object;
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license_plate_tracker);
 
-        getPicButton = (Button)findViewById(R.id.getPicture);
+        pic = (ImageView)findViewById(R.id.imageView1);
+        cameraObject = isCameraAvailiable();
+        //cameraObject = Camera.open();
+        showCamera = new ShowCamera(this, cameraObject);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(showCamera);
     }
 
-    public void open(){
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 0);
+    public void snapIt(View view){
+        cameraObject.takePicture(null, null, capturedIt);
     }
+
+    private Camera.PictureCallback capturedIt = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            if(bitmap==null){
+                Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();
+            }
+            cameraObject.release();
+        }
+    };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -55,11 +99,5 @@ public class LicensePlateTracker extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /** Called when the user clicks the Send button */
-    public void getPicture(View view) {
-        open();
-    }
-
 
 }
