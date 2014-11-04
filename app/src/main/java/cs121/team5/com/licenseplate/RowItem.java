f1b2d9c77e437840848d84e387fa319a923dac4c
PerryHolen <perry.holen@gmail.com>
2014-11-03 21:45:34 -0800

    added new files for custom adapter

---
+++ app/src/main/java/cs121/team5/com/licenseplate/RowItem.java
@@ -0,0 +1,38 @@
+package cs121.team5.com.licenseplate;
+
+/**
+ * Created by perry_000 on 11/3/2014.
+ */
+public class RowItem {
+    private int imageId;
+    private String title;
+    private String desc;
+
+    public RowItem(int imageId, String title, String desc) {
+        this.imageId = imageId;
+        this.title = title;
+        this.desc = desc;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @Override
+    public String toString() {
+        return title + "\n" + desc;
+    }
+}
